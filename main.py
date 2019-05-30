5d347c8d51e810fcca0d6cb79d93c78cc1d6d202
KritTangcharoenkijkul <48550332+KritTangcharoenkijkul@users.noreply.github.com>
2019-05-30 13:32:43 +0700

    Main function button to Register and Menu

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import sys
 
+from LearnerMenu import LearnerMenu
+from Register import Register
 from screen.signin import Ui_Form
 from PySide2.QtCore import *
 from PySide2.QtWidgets import *
 from PySide2.QtGui import *
 from PySide2.QtMultimedia import QSound
+
 
 class SignInUI(QWidget):
     def __init__(self):
@@ -14,6 +17,25 @@ class SignInUI(QWidget):
         self.setWindowTitle("Menu")
         self.ui = Ui_Form()
         self.ui.setupUi(self)
+        self.ui.pushButton_2.clicked.connect(self.BeLrn)
+        self.ui.pushButton_3.clicked.connect(self.BeOrg)
+        self.ui.pushButton.clicked.connect(self.Login)
+
+    def Login(self):
+        self.hide()
+        self.LrnMenu = LearnerMenu()
+        self.LrnMenu.show()
+
+
+    def BeOrg(self):
+        self.hide()
+        self.Register = Register(self)
+        self.Register.show()
+
+    def BeLrn(self):
+        self.hide()
+        self.Register = Register(self)
+        self.Register.show()
 
 
 if __name__ == '__main__':
